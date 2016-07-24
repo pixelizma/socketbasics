3f68c7e1bfd4fcf98fa413e4660b2bc315710b6d
Selçuk Oğuz <selcuk.oguz@hotmail.com.tr>
2016-07-24 12:15:25 +0300

    sending single message

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,14 @@ socket.on('connect', function() {
 socket.on('message', function(message){
 	console.log(message.text);
 });
+
+var $form = $('#message-form');
+
+$form.on('submit', function(event){
+	event.preventDefault();
+	var $message = $form.find('textarea[name=message]')
+	socket.emit('message', {
+		text: $message.val()
+	});
+	$message.val('');
+});
