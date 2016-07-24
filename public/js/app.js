b1bd0b2ca0634078ad134585dfdf442c9d25e399
Selçuk Oğuz <selcuk.oguz@hotmail.com.tr>
2016-07-24 23:18:22 +0300

    Query params add

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
+var name = getQueryVariable('name');
+var room = getQueryVariable('room');
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function() {
 	console.log('connect');
